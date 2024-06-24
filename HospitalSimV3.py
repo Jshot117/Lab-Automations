4a63dd3ade60e1c8b94e02dc73b5647574435ce4
Jshot117 <jonnjstart@gmail.com>
2024-06-24 13:59:12 -0400

    updated initalization of wells

--- HospitalSimV3.py
+++ HospitalSimV3.py
@@ -20,11 +20,11 @@ P300_TIPRACK_LOADNAME = "opentrons_96_filtertiprack_200ul"
 well_plate_patient_SLOT = "4"
 well_plate_patient_LOADNAME = "opentronsappliedbiosystems_96_aluminumblock_200ul"  # need to update loadname value to correct value
 
-well_plate_shift_one_SLOT = "5"
-well_plate_shift_one_LOADNAME = "opentronsappliedbiosystems_96_aluminumblock_200ul"  # need to update loadname value to correct value
+well_plate_shift_SLOT = "5"
+well_plate_shift_LOADNAME = "opentronsappliedbiosystems_96_aluminumblock_200ul"  # need to update loadname value to correct value
 
-well_plate_shift_two_SLOT = "6"
-well_plate_shift_two_LOADNAME = "opentronsappliedbiosystems_96_aluminumblock_200ul"  # need to update loadname value to correct value
+well_plate_second_patient_SLOT = "6"
+well_plate_second_patient_LOADNAME = "opentronsappliedbiosystems_96_aluminumblock_200ul"  # need to update loadname value to correct value
 
 well_plate_surfaces_SLOT = "8"
 well_plate_surfaces_LOADNAME = "opentronsappliedbiosystems_96_aluminumblock_200ul"  # need to update loadname value to correct value
@@ -69,7 +69,16 @@ categories = {
     "surfaces": [],
 }
 
-shifts = ["shift_one", "shift_two"]
+interaction_probabilities = {
+    "nurse_patient": 0.50,
+    "doctor_patient": 0.30,
+    "nurse_equipment": 0.20,
+    "doctor_equipment": 0.15,
+    "nurse_surface": 0.10,
+    "doctor_surface": 0.05,
+}
+
+shifts = ["shift_one", "shift_two", "shift_three"]
 
 
 zone_five = -40
@@ -97,11 +106,11 @@ def run(protocol: protocol_api.ProtocolContext):
     well_plate_patient = protocol.load_labware(
         well_plate_patient_LOADNAME, well_plate_patient_SLOT
     )
-    well_plate_shift_one = protocol.load_labware(
-        well_plate_shift_one_LOADNAME, well_plate_shift_one_SLOT
+    well_plate_shift = protocol.load_labware(
+        well_plate_shift_LOADNAME, well_plate_shift_SLOT
     )
-    well_plate_shift_two = protocol.load_labware(
-        well_plate_shift_two_LOADNAME, well_plate_shift_two_SLOT
+    well_plate_second_patient = protocol.load_labware(
+        well_plate_second_patient_LOADNAME, well_plate_second_patient_SLOT
     )
     well_plate_surfaces = protocol.load_labware(
         well_plate_surfaces_LOADNAME, well_plate_surfaces_SLOT
@@ -114,13 +123,15 @@ def run(protocol: protocol_api.ProtocolContext):
     categories["patients"] = well_plate_patient.wells[:20]
     
     categories["doctors"] = {
-        "shift_one": well_plate_shift_one.wells()[:6],
-        "shift_two": well_plate_shift_two.wells()[:6]
+        "shift_one": well_plate_shift.wells()[:6],
+        "shift_two": well_plate_second_patient.wells()[18:24],
+        "shift_three" : well_plate_shift.well()[36:42]
     }
     
     categories["nurses"] = {
-        "shift_one": well_plate_shift_one.wells()[6:18],
-        "shift_two": well_plate_shift_two.wells()[6:18]
+        "shift_one": well_plate_shift.wells()[6:18],
+        "shift_two": well_plate_shift.wells()[24:36],
+        "shift_three" : well_plate_shift.well()[42:54]
     }
 
     categories["equipment"] = well_plate_equipment.wells()[:20]
@@ -140,57 +151,68 @@ def run(protocol: protocol_api.ProtocolContext):
     # name common variables
     source_well = tuberack.wells()[0]
     source_well_volume = tuberack.wells()[0].max_volume
-    patient_zero_well = well_plate_patient.wells()[0]  # location for patient zero
+    patient_zero_well = categories["patients"][0]  # location for patient zero
     patient_zero_well_volume = initial_bacteria_amount
     # Creates and sets all plate wells to volume of zero
     well_plate_morning_volume = {k: 0 for k in well_plate_patient.wells()}
 
-    # Creates a list from dictionary of probabilites
-    column_probabilites_list = list(column_probabilites.values())
-    well_probabilites = []  # Creates a empty List
-    # fills empty well_probabilites list with 96 items and probabilites
-    for prob in column_probabilites_list:
-        well_probabilites.extend([prob] * 8)
-    well_probabilites[0] = 0
+    # # Creates a list from dictionary of probabilites
+    # column_probabilites_list = list(column_probabilites.values())
+    # well_probabilites = []  # Creates a empty List
+    # # fills empty well_probabilites list with 96 items and probabilites
+    # for prob in column_probabilites_list:
+    #     well_probabilites.extend([prob] * 8)
+    # well_probabilites[0] = 0
+    
     # start of commands
     right_pipette.transfer(
         initial_bacteria_amount, source_well.top(zone_five), patient_zero_well
     )
     source_well_volume = source_well_volume - initial_bacteria_amount
-    for i in range(4):
-        random_target = random.choices(
-            well_plate_patient.wells(), weights=well_probabilites, k=1
-        )[0]
-        while well_plate_morning_volume[random_target] >= 50:
-            random_target_index = well_plate_patient.wells().index(random_target)
-            well_probabilites[random_target_index] = 0
-            # Check if there's no valid well left
-            if sum(well_probabilites) == 0:
-                protocol.comment("All wells are full. Stopping transfer.")
-                return
-            random_target = random.choices(
-                well_plate_patient.wells(), weights=well_probabilites, k=1
-            )[0]
-        left_pipette.transfer(
-            bacteria_transfer_amount,
-            patient_zero_well,
-            random_target,
-            pick_up_tip="once",
-        )
-        well_plate_morning_volume[random_target] += bacteria_transfer_amount
-        patient_zero_well_volume = patient_zero_well_volume - bacteria_transfer_amount
-        aspiration_zone = determine_aspiration_zone(source_well_volume)
-        if aspiration_zone == "bottom":
-            source_well_aspiration_zone = source_well
-        else:
-            source_well_aspiration_zone = source_well.top(aspiration_zone)
-        if patient_zero_well_volume <= 0:
-            right_pipette.transfer(
-                initial_bacteria_amount,
-                source_well_aspiration_zone,
-                patient_zero_well,
-                pick_up_tip="once",
-            )
-            patient_zero_well_volume = initial_bacteria_amount
-            source_well_volume = source_well_volume - initial_bacteria_amount
-        time.sleep(time_amount)
+    
+    def simulate_interaction(source_well, target_well):
+        # Simulate the transfer of bacteria from source_well to target_well
+        left_pipette.transfer(bacteria_transfer_amount, source_well, target_well, new_tip='always')
+        protocol.delay(seconds=time_amount)
+    
+    
+    
+    
+    
+    # for i in range(4):
+    #     random_target = random.choices(
+    #         well_plate_patient.wells(), weights=well_probabilites, k=1
+    #     )[0]
+    #     while well_plate_morning_volume[random_target] >= 50:
+    #         random_target_index = well_plate_patient.wells().index(random_target)
+    #         well_probabilites[random_target_index] = 0
+    #         # Check if there's no valid well left
+    #         if sum(well_probabilites) == 0:
+    #             protocol.comment("All wells are full. Stopping transfer.")
+    #             return
+    #         random_target = random.choices(
+    #             well_plate_patient.wells(), weights=well_probabilites, k=1
+    #         )[0]
+    #     left_pipette.transfer(
+    #         bacteria_transfer_amount,
+    #         patient_zero_well,
+    #         random_target,
+    #         pick_up_tip="once",
+    #     )
+    #     well_plate_morning_volume[random_target] += bacteria_transfer_amount
+    #     patient_zero_well_volume = patient_zero_well_volume - bacteria_transfer_amount
+    #     aspiration_zone = determine_aspiration_zone(source_well_volume)
+    #     if aspiration_zone == "bottom":
+    #         source_well_aspiration_zone = source_well
+    #     else:
+    #         source_well_aspiration_zone = source_well.top(aspiration_zone)
+    #     if patient_zero_well_volume <= 0:
+    #         right_pipette.transfer(
+    #             initial_bacteria_amount,
+    #             source_well_aspiration_zone,
+    #             patient_zero_well,
+    #             pick_up_tip="once",
+    #         )
+    #         patient_zero_well_volume = initial_bacteria_amount
+    #         source_well_volume = source_well_volume - initial_bacteria_amount
+    #     time.sleep(time_amount)
